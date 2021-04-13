8d87caa709786833335e273dba69b764ae22659c
RobbG <RobbGlietz@gmail.com>
2021-04-12 17:22:31 -0700

    Started building out maxtrix of data used

--- Dashboard.py
+++ Dashboard.py
@@ -1,7 +1,20 @@
 import streamlit as st
+import pandas as pd
 
-st.title("This is the title")
-st.header("This is a header")
-st.subheader("This is a subheader")
-st.write("This is regualr text")
+def main():
+    FOLDER = 'D:\\Competitor_Analysis\\Done\\Yucaipa_Q1_Cleaned.csv'
+    df = pd.read_csv(FOLDER)
+
+    transactions = df.count()[0]
+    Ave_Days_On_Market = df['DaysOnMarket'].mean().round(decimals=0)
+    Max_Days_On_Market = df['DaysOnMarket'].max().round(decimals=0)
+    temp_closed = df['ClosePrice'].sum()
+    total_dollars_closed = "${:,.2f}".format(temp_closed)
+
+    print('Total dollars closed :',total_dollars_closed)
+    print('Avenue days on market :', int(Ave_Days_On_Market))
+    print('Max days on market :',int(Max_Days_On_Market))
+    print('Number of transactions :', transactions)
+if __name__ == '__main__':
+    main()
 
