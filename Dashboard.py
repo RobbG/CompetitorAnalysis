dcebbbda1e50087e50906064f72ae98f5f68f91f
RobbG <RobbGlietz@gmail.com>
2021-04-28 17:18:59 -0700

    Updated to show several different KPIs in Streamlit

--- Dashboard.py
+++ Dashboard.py
@@ -6,15 +6,31 @@ def main():
     df = pd.read_csv(FOLDER)
 
     transactions = df.count()[0]
-    Ave_Days_On_Market = df['DaysOnMarket'].mean().round(decimals=0)
-    Max_Days_On_Market = df['DaysOnMarket'].max().round(decimals=0)
+    ave_closed_sales_price = df['ClosePrice'].mean().round(decimals=0)
+    ave_days_on_market = df['DaysOnMarket'].mean().round(decimals=0)
+    max_days_on_market = df['DaysOnMarket'].max().round(decimals=0)
+    min_days_on_market = df['DaysOnMarket'].min().round(decimals=0)
     temp_closed = df['ClosePrice'].sum()
-    total_dollars_closed = "${:,.2f}".format(temp_closed)
+    total_dollars_closed = "$ {:,.2f}".format(temp_closed)
+    Broker_Listings = df['ListOfficeName'].value_counts()
+    Property_Types = df['PropertySubType'].value_counts()
+    buyer_agent_percentage = df['BuyerAgencyCompensation'].mean().round(decimals=0)
+    buyer_agent_commission = df['Buyer Agent Commission'].round(decimals=0)
 
-    print('Total dollars closed :',total_dollars_closed)
-    print('Avenue days on market :', int(Ave_Days_On_Market))
-    print('Max days on market :',int(Max_Days_On_Market))
-    print('Number of transactions :', transactions)
+    st.sidebar.write('Sidebar area.')
+
+
+    header = st.beta_container()
+    with header:
+        st.title('Competitive Analysis Quarter Based')
+        st.write('Total transactions for 1st QTR', transactions)
+        st.write('Avenue days on market', int(ave_days_on_market))
+        st.write('Total Dollars Closed', total_dollars_closed)
+        st.write('Max Days on Market', max_days_on_market)
+        st.write('Min Days on Market', min_days_on_market)
+        st.write('Ave Days on Market', int(ave_days_on_market))
+
+
 if __name__ == '__main__':
     main()
 
