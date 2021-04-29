e22b5338f956442c18888cae85e6db2404fc68d3
RobbG <RobbGlietz@gmail.com>
2021-04-28 17:17:38 -0700

    Updated file to load data and change headers and produce new file.

--- CSV_Loader.py
+++ CSV_Loader.py
@@ -4,19 +4,28 @@ Column_Names = ['City','ListAgentFirstName','ListAgentLastName','ListOfficeName'
                 'BuyerAgentLastName','BuyerAgencyCompensation','BuyerOfficeName','OriginalListPrice',
                 'ClosePrice','DaysOnMarket','CloseDate','ListingId','PropertySubType']
 
+QTR =""
 INPUT_FOLDER = 'D:\Competitor_Analysis\Pre_Loader\Yucaipa_Q1.csv'
 OUTPUT_FOLDER = 'D:\Competitor_Analysis\Done\Yucaipa_Q1_Cleaned.csv'
 def main():
     df = pd.read_csv(INPUT_FOLDER, usecols=Column_Names)
+
     df['ListAgentFirstName'] = df['ListAgentFirstName'].str.capitalize()
     df['ListAgentLastName'] = df['ListAgentLastName'].str.capitalize()
+    df["Listing Agent Name"] = df["ListAgentFirstName"].str.cat(df["ListAgentLastName"], sep=" ")
+    df = df.drop(["ListAgentFirstName", "ListAgentLastName"], axis=1)
     df['ListOfficeName'] = df['ListOfficeName'].str.capitalize()
     df['ListOfficeName'] = df['ListOfficeName'].str.title()
     df['BuyerAgentFirstName'] = df['BuyerAgentFirstName'].str.capitalize()
     df['BuyerAgentLastName'] = df['BuyerAgentLastName'].str.capitalize()
+    df["Buyer Agent Name"] = df["BuyerAgentFirstName"].str.cat(df["BuyerAgentLastName"], sep=" ")
+    df = df.drop(["BuyerAgentFirstName", "BuyerAgentLastName"], axis=1)
     df['BuyerOfficeName'] = df['BuyerOfficeName'].str.title()
-    df['CloseDate'] = pd.to_datetime(df['CloseDate']).dt.date
-
+    df['Month'] = pd.DatetimeIndex(df['CloseDate']).month
+    df['Year'] = pd.DatetimeIndex(df['CloseDate']).year
+    df = df.drop(["CloseDate"], axis=1)
+    df['Buyer Agent Commission'] = df['ClosePrice'] * df['BuyerAgencyCompensation']/100
+    df['Buyer Agent Commission'] = df['Buyer Agent Commission'].astype(int)
     df.to_csv(OUTPUT_FOLDER, index=False)
 
 if __name__ == '__main__':
