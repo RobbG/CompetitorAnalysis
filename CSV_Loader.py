47a71d033f8e8443f3fa2ab7a875ac3e7704569c
RobbG <RobbGlietz@gmail.com>
2021-04-29 17:15:05 -0700

    Updated parameters to clean data

--- CSV_Loader.py
+++ CSV_Loader.py
@@ -10,17 +10,17 @@ OUTPUT_FOLDER = 'D:\Competitor_Analysis\Done\Yucaipa_Q1_Cleaned.csv'
 def main():
     df = pd.read_csv(INPUT_FOLDER, usecols=Column_Names)
 
-    df['ListAgentFirstName'] = df['ListAgentFirstName'].str.capitalize()
-    df['ListAgentLastName'] = df['ListAgentLastName'].str.capitalize()
+    df['ListAgentFirstName'] = df['ListAgentFirstName'].str.lower()
+    df['ListAgentLastName'] = df['ListAgentLastName'].str.lower()
     df["Listing Agent Name"] = df["ListAgentFirstName"].str.cat(df["ListAgentLastName"], sep=" ")
     df = df.drop(["ListAgentFirstName", "ListAgentLastName"], axis=1)
-    df['ListOfficeName'] = df['ListOfficeName'].str.capitalize()
-    df['ListOfficeName'] = df['ListOfficeName'].str.title()
-    df['BuyerAgentFirstName'] = df['BuyerAgentFirstName'].str.capitalize()
-    df['BuyerAgentLastName'] = df['BuyerAgentLastName'].str.capitalize()
+    df['ListOfficeName'] = df['ListOfficeName'].str.lower()
+    df['ListOfficeName'] = df['ListOfficeName'].str.lower()
+    df['BuyerAgentFirstName'] = df['BuyerAgentFirstName'].str.lower()
+    df['BuyerAgentLastName'] = df['BuyerAgentLastName'].str.lower()
     df["Buyer Agent Name"] = df["BuyerAgentFirstName"].str.cat(df["BuyerAgentLastName"], sep=" ")
     df = df.drop(["BuyerAgentFirstName", "BuyerAgentLastName"], axis=1)
-    df['BuyerOfficeName'] = df['BuyerOfficeName'].str.title()
+    df['BuyerOfficeName'] = df['BuyerOfficeName'].str.lower()
     df['Month'] = pd.DatetimeIndex(df['CloseDate']).month
     df['Year'] = pd.DatetimeIndex(df['CloseDate']).year
     df = df.drop(["CloseDate"], axis=1)
